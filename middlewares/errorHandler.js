cdaa98419a0e195ef18c0b96611691d1f4491112
Gerry <150305559+iss4gerry@users.noreply.github.com>
2025-01-25 06:09:50 +0700

    feat/fix error handler

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,14 +1,20 @@
-module.exports = (error, req, res, next) => {
-	if (error instanceof multer.MulterError && error.code === 'LIMIT_FILE_SIZE') {
-		return res.status(413).json({
-			status: 'fail',
-			message: 'Payload content length greater than maximum allowed: 1000000',
-		});
-	} else if (error) {
-		return res.status(500).json({
-			status: 'fail',
-			message: error.message || 'An unknown error occurred',
-		});
-	}
-	next();
+import { Request, Response, NextFunction } from 'express';
+import multer from 'multer';
+
+const errorHandler = (error: any, req: Request, res: Response, next: NextFunction): void => {
+  if (error instanceof multer.MulterError && error.code === 'LIMIT_FILE_SIZE') {
+    res.status(413).json({
+      status: 'fail',
+      message: 'Payload content length greater than maximum allowed: 1000000',
+    });
+  } else if (error) {
+    res.status(500).json({
+      status: 'fail',
+      message: error.message || 'An unknown error occurred',
+    });
+  } else {
+    next();
+  }
 };
+
+export default errorHandler;
